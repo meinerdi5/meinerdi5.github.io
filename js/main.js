28c7dd58d108906f6b0cdb46c565ac7ac4b4e537
Anichin Pavel <meinerdi@gmail.com>
2017-03-26 23:11:38 +0300

    слайдер ёпт

--- js/main.js
+++ js/main.js
@@ -8,6 +8,35 @@ $container.masonry({
 $(document).ready(function(){
     $('.slider').slick({
         dots: true,
-        autoplay: true
-    });
+        infinite: true,
+        speed: 300,
+        slidesToShow: 6,
+        slidesToScroll: 3,
+        arrows: false,
+        responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
 });
